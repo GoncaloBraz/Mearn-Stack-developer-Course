f8f4b382742d337f1c84dea200ba38bd32772df7
GoncaloBraz <goncalonunopalmabraz@gmail.com>
2018-10-08 16:41:35 +0100

    1. server config & connection to database

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    mongoURI: 'mongodb://gbraz:Gb022009@ds125263.mlab.com:25263/gbraz_devconnector'
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then( () => {
+        console.log('MongoDB Connected')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.get('/', (req, res, next) => {
+    res.send('Hello')
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log('server running on port' + port)
+})
+
