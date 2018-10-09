d9e6e5a249b51eabb37fcd7161334ad42503f15a
GoncaloBraz <goncalonunopalmabraz@gmail.com>
2018-10-09 18:06:07 +0100

    14. Validation handlers - part 1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    mongoURI: 'mongodb://gbraz:Gb022009@ds125263.mlab.com:25263/gbraz_devconnector'
+    mongoURI: 'mongodb://gbraz:Gb022009@ds125263.mlab.com:25263/gbraz_devconnector',
+    secretKey: 'secret'
 
 }
