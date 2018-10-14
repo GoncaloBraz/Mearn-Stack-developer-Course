543aa248c74338d77e235dc6271ed0496fd35d1f
Gonçalo <goncalonunopalmabraz@gmail.com>
2018-10-15 00:02:04 +0100

    Profile Model - Section 4 lesson 17

--- routes/api/users-auth.js
+++ routes/api/users-auth.js
@@ -83,6 +83,12 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
 
+    const{errors, isValid} = ValidateLoginInput(req.body)
+    //Check validation
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
     const email = req.body.email
     const password = req.body.password
 
@@ -92,9 +98,8 @@ router.post('/login', (req, res) => {
     .then(user => {
         // Check for user
         if(!user){
-            return res.status(404).json({
-                email: 'User email not found'
-            })
+            errors.email = 'User not found' 
+            return res.status(404).json(errors)
         }
         // Check password
 
@@ -121,9 +126,8 @@ router.post('/login', (req, res) => {
                     }
                     )
             }else{
-                return res.status(400).json({
-                    password: 'password incorrect'
-                })
+                errors.password = 'Password incorrect' 
+                return res.status(400).json(errors)
             }
         })
 
