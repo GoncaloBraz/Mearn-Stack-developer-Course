3ca1f6196c15b3dc8e7563ca65e636fab72d3c23
Gonçalo <goncalonunopalmabraz@gmail.com>
2018-11-06 22:43:58 +0000

    25. Post Create Route (Falta testar com Postman)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const router  = express.Router()
-
+const router = express.Router()
+const passport = require("passport");
+const Post = require('../../models/Post')
 
 // @route   GET api/posts/test
 // @desc    Tests post route
@@ -8,6 +9,33 @@ const router  = express.Router()
 router.get('/test', (req, res) => res.json({
 
     message: 'posts works'
-}) )
+}))
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+
+router.post('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+
+    const {errors, isValid} = validatePostInput(req.body)
+
+    // Check Validation
+    if(!isValid){
+        // If any errors, send 400 with errors obj
+        return res.status(400).json(errors)
+    }
+
+    // CREATE OBJECT POST
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    })
+    // SAVE POST
+    newPost.save().then(post => res.json(post))
+})
 
 module.exports = router
